fcf99e248cb08616b1aa02cd76b24af376cdcf30
Mohit Aghera <mohitaghera0@gmail.com>
2024-06-12 19:09:21 +0530

    Remove Firebase Config

--- src/context/Firebase.jsx
+++ src/context/Firebase.jsx
@@ -4,14 +4,14 @@ import { getDatabase} from "firebase/database";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBKlNEF7at3XRgQ-J5hzdy3awcsjgRJ-gI",
-  authDomain: "massages-79084.firebaseapp.com",
-  projectId: "massages-79084",
-  storageBucket: "massages-79084.appspot.com",
-  messagingSenderId: "154262919267",
-  appId: "1:154262919267:web:ebed58f0fc1321c19c4056",
+  apiKey: "#",
+  authDomain: "#",
+  projectId: "#",
+  storageBucket: "#",
+  messagingSenderId: "#",
+  appId: "#",
   measurementId: "#",
-  databaseURL: "https://massages-79084-default-rtdb.firebaseio.com/",
+  databaseURL: "#",
 };
 
 const app = initializeApp(firebaseConfig);
